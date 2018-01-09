13262595d53b6af658e98dbd09d414b5bdef98fd
AbenezerMamo <ElitePowered@gmail.com>
2018-01-09 11:28:49 -0800

    petshop env setup

--- contracts/BotFactory.sol
+++ contracts/BotFactory.sol
@@ -1,42 +1,22 @@
-pragma solidity ^0.4.17;
+pragma solidity ^0.4.4;
+
+// http://truffleframework.com/tutorials/pet-shop
 
 contract BotFactory {
+  // array of 16 addresses where the key is the dogId
+  // and the value is the address of the account
+  // doing the adopting.
+  address[16] public adopters;
 
-    event NewBot(uint botID, uint dna);
-    event StartBattle(uint botID);
+  function adopt(uint petId) public returns (uint) {
+    require(petId >= 0 && petId <= 15);
 
-    function BotFactory() public {
-       botIndex = 0;
-   }
+    adopters[petId] = msg.sender; // the account who called this function
 
-   uint public botIndex;
+    return petId;
+  }
 
-   struct Bot {
-        uint id;
-        uint64 age; // timestamp of the block when the bot was born
-        uint256 genes; // genes that makeup the bot's aesthetic
-        uint64 energy; // power to battle another bot
-        uint64 reputation; // overall game success
-    }
-
-    Bot[] public bots;
-
-    mapping (uint => address) public botToOwner;
-    mapping (address => uint) public ownerToBot;
-    mapping (address => uint) ownerBotCount; // (owner address => num bots they own)
-
-    function _createBot(uint _dna) internal returns (uint) {
-        uint id = bots.push(Bot(botIndex, uint64(now), _dna, 1000, 0)) - 1;
-        botIndex++;
-        botToOwner[id] = msg.sender;
-        ownerToBot[msg.sender] = id;
-        ownerBotCount[msg.sender]++;
-        NewBot(id, _dna);
-        return id; 
-    }
-
-    function scanBotEnergy(uint id) public view returns(uint64){
-        uint64 energy = bots[id].energy;
-        return energy;
-    } 
+  function getAdopters() public returns (address[16]) {
+    return adopters;
+  }
 }
